933119fdbaf73dc6c95d08ce9f1e7d6d4ca53e29
w <none>
2023-03-12 01:54:52 +0000

    install: extract asset URL, bail if /tmp/nix exists

--- tmpnix/install.sh
+++ tmpnix/install.sh
@@ -3,13 +3,14 @@
 mkdir -p ~/tmpnix/logs
 touch ~/tmpnix/logs/build.txt
 
+NIX_ASSET=https://cdn.glitch.me/3f7fc1ee-0e69-41b0-980f-f32d1f89f671/nix-202302a.tar.gz?v=1677220350233
+NIX=/tmp/nix/store/fdwkwanm9dgcb1krcvz9cw1h4yz464yz-nix-2.11.1
+
 if [ ! -e /tmp/unpacked ]; then
-  rm -rf /tmp/nix
-  curl -L 'https://cdn.glitch.me/3f7fc1ee-0e69-41b0-980f-f32d1f89f671/nix-202302a.tar.gz?v=1677220350233' | tar -xzC /tmp
+  test ! -e /tmp/nix
+  curl -L "$NIX_ASSET" | tar -xzC /tmp
   touch /tmp/unpacked
 fi
-
-NIX=/tmp/nix/store/fdwkwanm9dgcb1krcvz9cw1h4yz464yz-nix-2.11.1
 
 . "$NIX/etc/profile.d/nix.sh"
 
