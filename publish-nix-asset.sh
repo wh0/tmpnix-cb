022fdbf54d8ac4031097600505a8b73e25587107
w <none>
2023-03-12 02:23:55 +0000

    upload-nix: rename to publish-nix-asset, bail, printout changes

--- publish-nix-asset.sh
+++ publish-nix-asset.sh
@@ -1,12 +1,16 @@
-#!/bin/sh -eux
+#!/bin/sh -eu
 printf 'output path(s) (e.g. /tmp/nix/store/xxx-nix-x.x.x): '
 read paths
 printf 'archive name (e.g. nix-yyyymmx.tar.gz): '
 read name
-rm -rf /tmp/pack
+set -x
+test ! -e /tmp/pack
 nix --extra-experimental-features nix-command copy --no-check-sigs --to /tmp/pack $paths
 tar -czvf "/tmp/$name" -C /tmp/pack nix
+ls -lh "/tmp/$name"
 snail ot request-join -r -p "$PROJECT_DOMAIN"
 url=$(snail asset push -p "$PROJECT_DOMAIN" "/tmp/$name")
 snail member leave -p "$PROJECT_DOMAIN"
-printf '%s\n' "$url"
+set +x
+printf 'NIX_ASSET=%s\n' "$url"
+printf 'NIX=%s\n' "$paths"
