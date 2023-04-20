022a2adb63633459f3bc76f85dd52d395136d1d8
w <none>
2023-04-20 04:38:35 +0000

    add get-drvs

--- watch-build.sh
+++ watch-build.sh
@@ -5,22 +5,7 @@ if [ ! -e /tmp/try ]; then
 fi
 rm /tmp/try
 
-if [ ! -e /tmp/top.txt ]; then
-  curl -fo /tmp/top.txt https://tmpnix-gsdrv.glitch.me/top.txt
-fi
-if [ ! -e /tmp/drvs.tar.gz ]; then
-  curl -fo /tmp/drvs.tar.gz https://tmpnix-gsdrv.glitch.me/drvs.tar.gz
-fi
-if [ ! -e /tmp/drvs ]; then
-  mkdir -p /tmp/drvs
-  tar -xf /tmp/drvs.tar.gz -C /tmp/drvs
-fi
-nix \
-  --extra-experimental-features nix-command \
-  copy \
-  --derivation \
-  --from file:///tmp/drvs \
-  $(cat /tmp/top.txt)
+~/get-drvs.sh
 
 outs=$(nix-store -rvk $(cat /tmp/top.txt))
 
