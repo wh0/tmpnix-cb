f7ef0bb298bbc3217a500d9c658dbea116a381dc
w <none>
2023-06-13 18:32:49 +0000

    maybe-cache: ignore OUT_PATHS

--- tmpnix/maybe-cache.sh
+++ tmpnix/maybe-cache.sh
@@ -3,17 +3,17 @@ if [ ! -e ~/.nix-profile ]; then
   # Some calls come before nix is even installed, e.g. building the user
   # environment that links to nix.
   printf >&2 'skipping %s before nix profile exists\n' "$DRV_PATH"
-  printf >>~/tmpnix/logs/build.txt '%s ! %s %s\n' "$(date)" "$DRV_PATH" "$OUT_PATHS"
+  printf >>~/tmpnix/logs/build.txt '%s ! %s %s\n' "$(date)" "$DRV_PATH" -
   exit 0
 fi
 
 if nix --extra-experimental-features nix-command show-derivation "$DRV_PATH" | grep -q preferLocalBuild; then
   printf >&2 'skipping %s for preferLocalBuild\n' "$DRV_PATH"
-  printf >>~/tmpnix/logs/build.txt '%s - %s %s\n' "$(date)" "$DRV_PATH" "$OUT_PATHS"
+  printf >>~/tmpnix/logs/build.txt '%s - %s %s\n' "$(date)" "$DRV_PATH" -
   exit 0
 fi
 
-printf >>~/tmpnix/logs/build.txt '%s + %s %s\n' "$(date)" "$DRV_PATH" "$OUT_PATHS"
+printf >>~/tmpnix/logs/build.txt '%s + %s %s\n' "$(date)" "$DRV_PATH" -
 
 printf >&2 'uploading %s\n' "$DRV_PATH"
-~/tmpnix/upload.sh $OUT_PATHS
+~/tmpnix/upload.sh "$DRV_PATH"
