ba1ae7f2377fc7c04d0678a081b4322d03da6721
w <none>
2023-10-29 05:39:53 +0000

    install: new nix asset

--- tmpnix/install.sh
+++ tmpnix/install.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/tmpnix/logs
 touch ~/tmpnix/logs/build.txt
 
-NIX_ASSET=https://cdn.glitch.me/e7db9df3-e53f-4dbb-8a80-8c10588888e3/nix-202310b.tar.gz?v=1697687110090
-NIX=/tmp/nix/store/d5wdpml2ar8q2psk07c6spg14qa4r48v-nix-2.13.5
+NIX_ASSET=https://cdn.glitch.me/e7db9df3-e53f-4dbb-8a80-8c10588888e3/nix-202310c.tar.gz?v=1698557962512
+NIX=/tmp/nix/store/ymj5396pr9vrxyr8nvl4l7l4c4qc3pic-nix-2.13.6
 
 if [ ! -e /tmp/unpacked ]; then
   test ! -e /tmp/nix
