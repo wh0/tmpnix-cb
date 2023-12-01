a49a76aea81a35be1aa5f43361f976bc180ea835
w <none>
2023-12-01 23:57:51 +0000

    overlays: update python psutil override

--- tmpnix/visible.config/nixpkgs/overlays/python-psutil.nix
+++ tmpnix/visible.config/nixpkgs/overlays/python-psutil.nix
@@ -1,7 +1,7 @@
 self: super:
 
 {
-  python310 = super.python310.override {
+  python311 = super.python311.override {
     packageOverrides = pself: psuper: {
       psutil = psuper.psutil.overrideAttrs (old: {
         patches = (old.patches or []) ++ [
