c7f5379fb6ac070cb5787ba933f530e8359a237a
w <none>
2024-06-10 06:13:55 +0000

    overlays: openexr_3 port threading patch

--- tmpnix/visible.config/nixpkgs/overlays/openexr.nix
+++ tmpnix/visible.config/nixpkgs/overlays/openexr.nix
@@ -6,4 +6,9 @@ self: super:
       ./openexr-test-threading.patch
     ];
   });
+  openexr_3 = super.openexr_3.overrideAttrs (old: {
+    patches = (old.patches or []) ++ [
+      ./openexr_3-test-threading.patch
+    ];
+  });
 }
