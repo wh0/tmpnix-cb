eb1eb6432769d89aa7f3fad5642c8e1b8c6dd35d
w <none>
2024-07-25 06:21:50 +0000

    overlays: fuss with nodejs deep override

--- tmpnix/visible.config/nixpkgs/overlays/nodejs.nix
+++ tmpnix/visible.config/nixpkgs/overlays/nodejs.nix
@@ -3,10 +3,12 @@ self: super:
 {
   nodejs_22 = super.nodejs_22.override {
     callPackage = p: args: self.callPackage p (args // {
-      stdenv = self.ccacheStdenv;
-      buildPackages = self.buildPackages // {
-        stdenv = self.buildPackages.ccacheStdenv; 
+      "nodejs.nix" = {
+        stdenv = self.ccacheStdenv;
+        buildPackages = self.buildPackages // {
+          stdenv = self.buildPackages.ccacheStdenv;
+        };
       };
-    });
+    }.${baseNameOf p} or {});
   };
 }
