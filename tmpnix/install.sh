35c331b56fa03dd5c92d844e9e141299a96a1830
w <none>
2024-09-27 04:49:58 +0000

    install: new nix asset

--- tmpnix/install.sh
+++ tmpnix/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
-NIX_ASSET=https://cdn.glitch.me/5a9fc653-d4ea-4f87-b41f-2015392bbd4a/nix-202409a.tar.gz?v=1726375100477
-NIX=/tmp/nix/store/d458mj32h8s4x1838r29wmmm113hfmcz-nix-2.18.5
+NIX_ASSET=https://cdn.glitch.me/5a9fc653-d4ea-4f87-b41f-2015392bbd4a/nix-202409b.tar.gz?v=1727412487790
+NIX=/tmp/nix/store/cxx11b1lyvf8kww7wiraiwzrsm442xck-nix-2.18.7
 
 if [ ! -e /tmp/nix ]; then
   if [ -e /tmp/unpack.tmp ]; then
