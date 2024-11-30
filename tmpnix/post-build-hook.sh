ed64238300b381a90f59632ba2db160f5464fcf1
w <none>
2024-11-30 19:21:39 +0000

    post-build-hook: only log if logs dir exists

--- tmpnix/post-build-hook.sh
+++ tmpnix/post-build-hook.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -eu
 if grep -q preferLocalBuild "$DRV_PATH"; then
   printf >&2 'skipping %s for preferLocalBuild\n' "$DRV_PATH"
-  printf >>~/tmpnix/logs/build.txt '%s - %s %s\n' "$(date)" "$DRV_PATH" -
+  test ! -d ~/tmpnix/logs || printf >>~/tmpnix/logs/build.txt '%s - %s %s\n' "$(date)" "$DRV_PATH" -
   exit 0
 fi
 
-printf >>~/tmpnix/logs/build.txt '%s H %s %s\n' "$(date)" "$DRV_PATH" -
+test ! -d ~/tmpnix/logs || printf >>~/tmpnix/logs/build.txt '%s H %s %s\n' "$(date)" "$DRV_PATH" -
 
 printf >&2 'uploading %s\n' "$DRV_PATH"
 ~/tmpnix/upload-inner.sh "$DRV_PATH^*"
