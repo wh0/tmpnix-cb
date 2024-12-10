2715a0f6cf5a29b49c1a8b0f5dc58baed70122b2
w <none>
2024-12-10 01:37:05 +0000

    install: put cache in /tmp

--- tmpnix/install.sh
+++ tmpnix/install.sh
@@ -2,6 +2,12 @@
 NIX_ASSET=https://cdn.glitch.me/5a9fc653-d4ea-4f87-b41f-2015392bbd4a/nix-202412a.tar.gz?v=1733374682759
 NIX=/tmp/nix/store/q90wgfy8ghix51qwxjjp92wv86cz8adh-nix-2.24.10
 
+if [ ! -e ~/.cache/nix ]; then
+  mkdir -p /tmp/nix-cache
+  mkdir -p ~/.cache
+  ln -s /tmp/nix-cache ~/.cache/nix
+fi
+
 if [ ! -e /tmp/nix ]; then
   if [ -e /tmp/unpack.tmp ]; then
     chmod -R +w /tmp/unpack.tmp
