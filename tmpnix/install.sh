238844cc7db78d7f7bd8f7610103db16afa65673
w <none>
2025-03-09 21:49:06 +0000

    install: create cache before testing if symlink resolves

--- tmpnix/install.sh
+++ tmpnix/install.sh
@@ -2,8 +2,8 @@
 NIX_ASSET=https://cdn.glitch.me/5a9fc653-d4ea-4f87-b41f-2015392bbd4a/nix-202502a.tar.gz?v=1740726597162
 NIX=/tmp/nix/store/zk4yrln3xsnh76zpmvrs85cifs5n6k43-nix-2.24.12
 
+mkdir -p /tmp/nix-cache
 if [ ! -e ~/.cache/nix ]; then
-  mkdir -p /tmp/nix-cache
   mkdir -p ~/.cache
   ln -s /tmp/nix-cache ~/.cache/nix
 fi
